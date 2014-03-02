026fb50eeb2501fdc0deb1a152fa7848d888855a
Raevn <d_bened@raevn.com>
2014-03-02 20:34:09 +1100

    Updated rBiggerArmoryImages to v1.1.0

---
+++ rBiggerArmoryImages/ui/mods/rBiggerArmoryImages/rBiggerArmoryImages.js
@@ -0,0 +1,7 @@
+﻿$('#tab_commanders .div_settings_graphics_cont').prepend(
+	'<div class="commander_preview_cont">' +
+		'<img class="commander_preview_image" src="" data-bind="attr: {src: model.selectedIndex() > -1 ? model.commanders()[model.selectedIndex()].ImgSource : \'\'}">' +
+		'<div class="commander_preview_name" data-bind="text: model.selectedIndex() > -1 ? model.commanders()[model.selectedIndex()].DisplayName : \'\'"></div>' +
+	'</div>' +
+	'<div class="commander_scroll"></div>');
+$('#tab_commanders .div_settings_graphics_cont table').appendTo('.commander_scroll');
