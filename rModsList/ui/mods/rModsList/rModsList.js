a016029a4dfbfbcbf8fa8e6ae3e16649f2b91509
mereth <mereth78@gmail.com>
2014-07-10 22:18:16 +0200

    reworked rModsList to trully remove the PAMM dependency

--- rModsList/ui/mods/rModsList/rModsList.js
+++ rModsList/ui/mods/rModsList/rModsList.js
@@ -1,59 +1,50 @@
-model.rModList = {};
+(function() {
+    model.rModList = {};
+    model.rModList.modItems = ko.observableArray([]);
+    model.rModList.updatesRequired = ko.observable(0);
 
-model.rModList.loadModJSON = function(src) {
-    console.log(src, "loading json");
-    var o = new XMLHttpRequest();
-	
-    try
-    {
-        o.open('GET', src, false);
-        o.send('');
-    }
-    catch( err )
-    {
-        console.log("error loading " + src)
-        return;
-    }
-    var jsonData = decode(o.responseText);
-	return jsonData;
-}
-
-model.rModList["modData"] = model.rModList.loadModJSON("coui://ui/mods/mods_list.json");
-model.rModList["onlineModData"] = model.rModList.loadModJSON("http://pamods.github.io/modlist.json");
-model.rModList["updatesRequired"] = ko.observable(0);
-
-var arrayData = [];
-	
-for (var id in model.rModList.modData) {
-	if (model.rModList.modData[id].enabled === true && id != "rPAMM") {
-		arrayData.push(model.rModList.modData[id]);
-		arrayData[arrayData.length - 1].author = arrayData[arrayData.length - 1].author ? arrayData[arrayData.length - 1].author : "";
-		arrayData[arrayData.length - 1].build = arrayData[arrayData.length - 1].build ? arrayData[arrayData.length - 1].build : "";
-		arrayData[arrayData.length - 1].display_name = arrayData[arrayData.length - 1].display_name ? arrayData[arrayData.length - 1].display_name : "";
-		arrayData[arrayData.length - 1].version = arrayData[arrayData.length - 1].version ? arrayData[arrayData.length - 1].version : "";
-		arrayData[arrayData.length - 1].icon = arrayData[arrayData.length - 1].icon ? arrayData[arrayData.length - 1].icon : "coui://ui/mods/rModsList/img/generic.png";
-		arrayData[arrayData.length - 1].forum = arrayData[arrayData.length - 1].forum ? arrayData[arrayData.length - 1].forum : "";
-		arrayData[arrayData.length - 1].date = arrayData[arrayData.length - 1].date ? arrayData[arrayData.length - 1].date : "";
-		arrayData[arrayData.length - 1].update_required = false;
-		
-		if (model.rModList.onlineModData[id] != null) {
-			if (arrayData[arrayData.length - 1].date < model.rModList.onlineModData[id].date) {
-				model.rModList.updatesRequired(model.rModList.updatesRequired() + 1);
-				arrayData[arrayData.length - 1].update_required = true;
-			}
-		}
-	}
-}
-
-model.rModList["modItems"] = ko.observableArray(arrayData);
-
-$('#sidebar-tabs').append('<li><a href="#mods" data-toggle="pill" data-bind="click_sound: \'default\', rollover_sound: \'default\'">Mods <!-- ko if: model.rModList.updatesRequired() > 0 --><img src="coui://ui/mods/rModsList/img/alert.png" height="14"><!-- /ko --></a></li>');
-
-$.get("coui://ui/mods/rModsList/rModsList.html", function (data) {
-	$('.tab-content').append(data); 
-	ko.applyBindings(model, document.getElementById("mods"));
-	
-	if (model.rModList.updatesRequired() > 0) {
-		$('#mods').find('.ytv-list-inner').css('top', '66px');
-	}
-});
+    $.getJSON(
+        "http://pamods.github.io/modlist2.json",
+        function(availableMods) {
+            var modsmap = {};
+            for(var i = 0; i <availableMods.length; ++i) {
+                var mod = availableMods[i];
+                modsmap[mod.identifier] = mod;
+            }
+            
+            api.mods.getMountedMods("client", function (mods) {
+                var updatesRequired = 0;
+                for(var i = 0; i < mods.length; ++i) {
+                    var mod = mods[i];
+                    
+                    var avmod = modsmap[mod.identifier];
+                    if(!avmod) avmod = {};
+                    
+                    mod.icon = avmod.icon ? avmod.icon : "coui://ui/mods/rModsList/img/generic.png";
+                    mod.update_required = avmod.version && mod.version !== avmod.version;
+                    
+                    model.rModList.modItems.push(mod);
+                    
+                    if(mod.update_required) {
+                        updatesRequired++;
+                    }
+                }
+                
+                model.rModList.modItems.sort(function(a, b) { return a.display_name === b.display_name ? 0 : (a.display_name < b.display_name ? -1 : 1) })
+                model.rModList.updatesRequired(updatesRequired);
+            });
+        },
+        'json'
+    );
+    
+    $('#sidebar-tabs').append('<li><a href="#mods" data-toggle="pill" data-bind="click_sound: \'default\', rollover_sound: \'default\'">Mods <img src="coui://ui/mods/rModsList/img/alert.png" height="14" data-bind="visible: model.rModList.updatesRequired() > 0"></a></li>');
+    
+    $.get("coui://ui/mods/rModsList/rModsList.html", function (data) {
+        $('.tab-content').append(data); 
+        ko.applyBindings(model, document.getElementById("mods"));
+        
+        if (model.rModList.updatesRequired() > 0) {
+            $('#mods').find('.ytv-list-inner').css('top', '66px');
+        }
+    });
+})();
